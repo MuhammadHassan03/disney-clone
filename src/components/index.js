6c4256471ad06c95cc5f947a0174867d5e815690
Muhammad Hassan <mh7722178@gmail.com>
2024-01-28 17:35:19 +0500

    disney-clone

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+
+
+export { default as Home } from "./Home";
+export { default as Header } from "./Header";
+export { default as Login } from "./Login";
+export { default as ImgSlider } from "./ImgSlider";
+export { default as Detail } from "./Detail";
+export { default as Viewers } from "./Viewers";
+export { default as NewDisney } from "./NewDisney";
+export { default as Originals } from "./Originals";
+export { default as Recommends } from "./Recommends";
+export { default as Trending } from "./Trending";
